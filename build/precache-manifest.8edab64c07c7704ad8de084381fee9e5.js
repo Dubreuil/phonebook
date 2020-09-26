0dd92f5a9c20697c0b315825317d040228972b26
Mathieu Dubreuil <dubm1811@usherbrooke.ca>
2020-09-26 15:04:24 -0400

    uibuild

---
+++ build/precache-manifest.8edab64c07c7704ad8de084381fee9e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0a3332809a093e8c276584b34da88f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9debf590f314acaa83bf",
+    "url": "/static/css/main.7ca56341.chunk.css"
+  },
+  {
+    "revision": "54dd32709c192453a110",
+    "url": "/static/js/2.d98781c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d98781c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9debf590f314acaa83bf",
+    "url": "/static/js/main.a495c97b.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
